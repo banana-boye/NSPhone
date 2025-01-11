0de9412ba1f808df0fa2d6d06b783c9393063557
banana-boye <bferraz1313@gmail.com>
2025-01-11 08:48:46 +0100

    first

---
+++ startup.lua
@@ -0,0 +1,19 @@
+-- Check if all files exist
+local version = "1"
+
+if not fs.exists("SI") then fs.makeDir("SI") end
+if http.get("https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/version.txt").readAll() ~= version then
+    for _, file in pairs(fs.list("SI")) do
+        if file then
+            shell.run("delete SI/"..file)
+        end
+    end
+    shell.run("delete startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/startup.lua startup.lua")
+    shell.run("reboot")
+end
+if not fs.exists("SI/basalt.lua") then shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua SI/basalt.lua") end
+if not fs.exists("SI/main.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/SI/main.lua SI/main.lua") end
+
+-- Run
+shell.run("SI/main.lua")
